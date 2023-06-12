6313e1069415d4098ac04fd9f15504b20e55f9bd
tonjetj <tontotjen@gmail.com>
2023-06-12 13:39:10 +0200

    adds login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import RegistrationForm from "./components/registration/Registration"
+import LoginForm from './components/login/Login';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="App">
+    <RegistrationForm/>
+    <LoginForm/>
+  </div>;
 }
 
 export default App;
