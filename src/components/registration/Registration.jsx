6b13240b04185b6b601dcc9f2aa006fad5b2878e
tonjetj <tontotjen@gmail.com>
2023-06-12 13:38:13 +0200

    added functionality to registration form

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { BASE_URL } from '../api/apiBase';
 
-import React from 'react';
+const schema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup.string().required('Password is required'),
+});
 
 function RegistrationForm() {
-  return <div>Registration</div>;
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch(`${BASE_URL}/auction/auth/register`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to register user');
+      }
+
+      const responseData = await response.json();
+      console.log('User registered successfully:', responseData); // remember to delete the responseData!!
+      // Continue with other actions
+    } catch (error) {
+      console.error('Failed to register user:', error);
+      // Handle error gracefully
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Name"
+        {...register('name')}
+      />
+      {errors.name && <p>{errors.name.message}</p>}
+      
+      <input
+        type="email"
+        placeholder="Email"
+        {...register('email')}
+      />
+      {errors.email && <p>{errors.email.message}</p>}
+      
+      <input
+        type="password"
+        placeholder="Password"
+        {...register('password')}
+      />
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <button>Register</button>
+    </form>
+  );
 }
 
-export default Registration;
+export default RegistrationForm;
